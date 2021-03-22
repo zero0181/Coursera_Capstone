0f8db664fb00b9e0322de310f83cf03c67b2978d
zero0181 <68711573+zero0181@users.noreply.github.com>
2021-03-22 20:54:22 +0700

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This Project part of IBM Data Sci Project
+# Created by supat
+
+# In[8]:
+
+
+import pandas as pd
+import numpy  as np
+
+
+# In[9]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
